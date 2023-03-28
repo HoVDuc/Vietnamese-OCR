b8b639fa56bd687cb84f2db9f641de482739c849
HoVDuc <hoduccity@gmail.com>
2023-03-28 16:09:03 +0700

    commit

--- TextRecognition/ocr/loader/dataloader.py
+++ TextRecognition/ocr/loader/dataloader.py
@@ -144,3 +144,90 @@ class OCRDataset(Dataset):
 
     def __len__(self):
         return self.n_samples
+
+class ClusterRandomSampler(Sampler):
+    
+    def __init__(self, data_source, batch_size, shuffle=True):
+        self.data_source = data_source
+        self.batch_size = batch_size
+        self.shuffle = shuffle        
+
+    def flatten_list(self, lst):
+        return [item for sublist in lst for item in sublist]
+
+    def __iter__(self):
+        batch_lists = []
+        for cluster, cluster_indices in self.data_source.cluster_indices.items():
+            if self.shuffle:
+                random.shuffle(cluster_indices)
+
+            batches = [cluster_indices[i:i + self.batch_size] for i in range(0, len(cluster_indices), self.batch_size)]
+            batches = [_ for _ in batches if len(_) == self.batch_size]
+            if self.shuffle:
+                random.shuffle(batches)
+
+            batch_lists.append(batches)
+
+        lst = self.flatten_list(batch_lists)
+        if self.shuffle:
+            random.shuffle(lst)
+
+        lst = self.flatten_list(lst)
+
+        return iter(lst)
+
+    def __len__(self):
+        return len(self.data_source)
+    
+class Collator(object):
+    def __init__(self, masked_language_model=True):
+        self.masked_language_model = masked_language_model
+
+    def __call__(self, batch):
+        filenames = []
+        img = []
+        target_weights = []
+        tgt_input = []
+        max_label_len = max(len(sample['word']) for sample in batch)
+        for sample in batch:
+            img.append(sample['img'])
+            filenames.append(sample['img_path'])
+            label = sample['word']
+            label_len = len(label)
+            
+            
+            tgt = np.concatenate((
+                label,
+                np.zeros(max_label_len - label_len, dtype=np.int32)))
+            tgt_input.append(tgt)
+
+            one_mask_len = label_len - 1
+
+            target_weights.append(np.concatenate((
+                np.ones(one_mask_len, dtype=np.float32),
+                np.zeros(max_label_len - one_mask_len,dtype=np.float32))))
+            
+        img = np.array(img, dtype=np.float32)
+
+
+        tgt_input = np.array(tgt_input, dtype=np.int64).T
+        tgt_output = np.roll(tgt_input, -1, 0).T
+        tgt_output[:, -1]=0
+        
+        # random mask token
+        if self.masked_language_model:
+            mask = np.random.random(size=tgt_input.shape) < 0.05
+            mask = mask & (tgt_input != 0) & (tgt_input != 1) & (tgt_input != 2)
+            tgt_input[mask] = 3
+
+        tgt_padding_mask = np.array(target_weights)==0
+
+        rs = {
+            'img': torch.FloatTensor(img),
+            'tgt_input': torch.LongTensor(tgt_input),
+            'tgt_output': torch.LongTensor(tgt_output),
+            'tgt_padding_mask': torch.BoolTensor(tgt_padding_mask),
+            'filenames': filenames
+        }   
+        
+        return rs
--- TextRecognition/ocr/tools/translate.py
+++ TextRecognition/ocr/tools/translate.py
@@ -4,6 +4,141 @@ import math
 from PIL import Image
 from torch.nn.functional import log_softmax, softmax
 
+from ocr.model.transformerocr import VietOCR
+from ocr.model.vocab import Vocab
+from ocr.model.beam import Beam
+
+def batch_translate_beam_search(img, model, beam_size=4, candidates=1, max_seq_length=128, sos_token=1, eos_token=2):
+    # img: NxCxHxW
+    model.eval()
+    device = img.device
+    sents = []
+
+    with torch.no_grad():
+        src = model.cnn(img)
+        print(src.shap)
+        memories = model.transformer.forward_encoder(src)
+        for i in range(src.size(0)):
+#            memory = memories[:,i,:].repeat(1, beam_size, 1) # TxNxE
+            memory = model.transformer.get_memory(memories, i)
+            sent = beamsearch(memory, model, device, beam_size, candidates, max_seq_length, sos_token, eos_token)
+            sents.append(sent)
+
+    sents = np.asarray(sents)
+
+    return sents
+
+def beamsearch(memory, model, device, beam_size=4, candidates=1, max_seq_length=128, sos_token=1, eos_token=2):    
+    # memory: Tx1xE
+    model.eval()
+
+    beam = Beam(beam_size=beam_size, min_length=0, n_top=candidates, ranker=None, start_token_id=sos_token, end_token_id=eos_token)
+
+    with torch.no_grad():
+#        memory = memory.repeat(1, beam_size, 1) # TxNxE
+        memory = model.transformer.expand_memory(memory, beam_size)
+
+        for _ in range(max_seq_length):
+            
+            tgt_inp = beam.get_current_state().transpose(0,1).to(device)  # TxN
+            decoder_outputs, memory = model.transformer.forward_decoder(tgt_inp, memory)
+
+            log_prob = log_softmax(decoder_outputs[:,-1, :].squeeze(0), dim=-1)
+            beam.advance(log_prob.cpu())
+            
+            if beam.done():
+                break
+                
+        scores, ks = beam.sort_finished(minimum=1)
+
+        hypothesises = []
+        for i, (times, k) in enumerate(ks[:candidates]):
+            hypothesis = beam.get_hypothesis(times, k)
+            hypothesises.append(hypothesis)
+    
+    return [1] + [int(i) for i in hypothesises[0][:-1]]
+
+def translate_beam_search(img, model, beam_size=4, candidates=1, max_seq_length=128, sos_token=1, eos_token=2):
+    # img: 1xCxHxW
+    model.eval()
+    device = img.device
+
+    with torch.no_grad():
+        src = model.cnn(img)
+        memory = model.transformer.forward_encoder(src) #TxNxE
+        sent = beamsearch(memory, model, device, beam_size, candidates, max_seq_length, sos_token, eos_token)
+
+    return sent
+
+def translate(img, model, max_seq_length=128, sos_token=1, eos_token=2):
+    """_summary_
+
+    Args:
+        img (torch): Ảnh đã được xử lý
+        model (_type_): Mô hình dùng để đánh giá
+        max_seq_length (int, optional): _description_. Defaults to 128.
+        sos_token (int, optional): _description_. Defaults to 1.
+        eos_token (int, optional): _description_. Defaults to 2.
+    """
+    "data: BxCxHxW"
+    model.eval()
+    device = img.device
+
+    with torch.no_grad():
+        src = model.cnn(img)
+        memory = model.transformer.forward_encoder(src)
+
+        translated_sentence = [[sos_token]*len(img)]
+        char_probs = [[1]*len(img)]
+
+        max_length = 0
+
+        while max_length <= max_seq_length and not all(np.any(np.asarray(translated_sentence).T==eos_token, axis=1)):
+
+            tgt_inp = torch.LongTensor(translated_sentence).to(device)
+            
+#            output = model(img, tgt_inp, tgt_key_padding_mask=None)
+#            output = model.transformer(src, tgt_inp, tgt_key_padding_mask=None)
+            output, memory = model.transformer.forward_decoder(tgt_inp, memory)
+            output = softmax(output, dim=-1)
+            output = output.to('cpu')
+
+            values, indices  = torch.topk(output, 5)
+            
+            indices = indices[:, -1, 0]
+            indices = indices.tolist()
+            
+            values = values[:, -1, 0]
+            values = values.tolist()
+            char_probs.append(values)
+
+            translated_sentence.append(indices)   
+            max_length += 1
+
+            del output
+
+        translated_sentence = np.asarray(translated_sentence).T
+        
+        char_probs = np.asarray(char_probs).T
+        char_probs = np.multiply(char_probs, translated_sentence>3)
+        char_probs = np.sum(char_probs, axis=-1)/(char_probs>0).sum(-1)
+    
+    return translated_sentence, char_probs
+
+
+def build_model(config):
+    vocab = Vocab(config['vocab']) #Load bộ vocab từ config
+    device = config['device'] #Load cuda hoặc cpu
+    
+    model = VietOCR(len(vocab),
+            config['backbone'],
+            config['cnn'], 
+            config['transformer'],
+            config['seq_modeling']) #Load model VietOCR
+    
+    model = model.to(device) 
+
+    return model, vocab 
 
 def resize(w, h, expected_height, image_min_width, image_max_width):
     # Tính new width dựa vào expected height
@@ -43,3 +178,30 @@ def process_image(image, image_height, image_min_width, image_max_width):
     img = np.asarray(img).transpose(2, 0, 1)
     img = img/255  # scale lại ảnh
     return img
+
+def process_input(image, image_height, image_min_width, image_max_width):
+    img = process_image(image, image_height, image_min_width, image_max_width)
+    img = img[np.newaxis, ...] # Thêm một chiều mới vào ảnh
+    img = torch.FloatTensor(img) # Convert ảnh từ numpy sang torch float
+    return img
+
+def predict(filename, config):
+    """_summary_
+
+    Args:
+        filename (str): Đường dẫn của ảnh
+        config (_type_):
+
+    Returns:
+        str: Kết quả dự đoán
+    """
+    img = Image.open(filename) # Load ảnh
+    img = process_input(img) #Xử lý đầu vào
+
+    img = img.to(config['device'])
+
+    model, vocab = build_model(config) # Load model và bộ vocab
+    s = translate(img, model)[0].tolist()
+    s = vocab.decode(s)
+    
+    return s
