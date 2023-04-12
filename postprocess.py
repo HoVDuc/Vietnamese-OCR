448f6fc3cf1b16e429f2cf188c5e7661fadfc98b
HoVDuc <hoducity@gmail.com>
2023-04-12 13:31:09 +0700

    add postprocess

---
+++ postprocess.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import pyclipper
+from shapely.geometry import Polygon
+
+class PostProcess:
+
+    def __init__(self) -> None:
+        pass
--- test.py
+++
@@ -1,6 +0,0 @@
-import sys
-import time
-  
-for i in range(10):
-    print(i, end =' ', flush=False)
-    time.sleep(1)
