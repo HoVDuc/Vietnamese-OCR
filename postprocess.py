0b2abfe878450ab689e0c49065f0d4c3927023b2
HoVDuc <hoduccity@gmail.com>
2023-04-16 09:19:01 +0000

    add perspective

--- postprocess.py
+++ postprocess.py
@@ -57,15 +57,13 @@ class PostProcess:
     
         return np.array(boxes)
     
-    def cut_img(self, img, contour):
+    def get_size(self, contour):
         x1, x2, x3, x4 = contour
         x = min(x1[0], x4[0])
         y = min(x1[1], x2[1])
         w = max(x2[0], x3[0]) - x
         h = max(x4[1], x3[1]) - y
-        img = img[y:y+h, x:x+w]
-        
-        return img if np.any(img) else None
+        return h, w
         
     def get_img(self):
         imgs = []
@@ -75,10 +73,39 @@ class PostProcess:
                 imgs.append(im)
         return imgs
     
+    def reorder(self, points):
+        points = points.reshape((4, 2))
+        new_points = np.zeros((4, 1, 2), dtype=np.int32)
+        add = points.sum(1)
+        new_points[0] = points[np.argmin(add)]
+        new_points[3] = points[np.argmax(add)]
+        diff = np.diff(points, axis=1)
+        new_points[1] = points[np.argmin(diff)]
+        new_points[2] = points[np.argmax(diff)]
+        return new_points
+    
+    def get_Perspective(self, img, masked, location, size: tuple):
+        """Takes original image as input"""
+        height, width = size
+        pts1 = np.float32(location)
+        pts2 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
+        
+        # Apply Perspective Transform Algorithm
+        matrix = cv2.getPerspectiveTransform(pts1, pts2)
+        result = cv2.warpPerspective(masked, matrix, (width, height))
+        return result
+    
     def __call__(self, image, contours, unclip_ratio=0.6):
         self.image = image
         self.contours = contours
         self.unclip_ratio = unclip_ratio
 
         self.contours = self.post_processing()
-        return self.get_img()
+        imgs = []
+        for contour in self.contours:
+            size = self.get_size(contour)
+            location = self.reorder(contour)
+            imgWrap = self.get_Perspective(self.image, self.image, location, size)
+            imgs.append(imgWrap)
+
+        return imgs
